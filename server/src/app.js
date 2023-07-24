61bf03f53706d8315e893b9811c152821d13d89c
Dustin <dustindung300@gmail.com>
2023-07-24 10:06:11 +0700

    Serving React.js front end In production.

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const cors = require("cors");
 
@@ -10,7 +12,10 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
 
 module.exports = app;
